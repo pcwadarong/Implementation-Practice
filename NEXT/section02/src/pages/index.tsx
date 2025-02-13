5f3c31d29985f8e2c60a93cbd85d5ffd536cc4f0
Chaewon <cw06155@gmail.com>
2025-02-14 02:44:14 +0900

    server side props

--- NEXT/section02/src/pages/index.tsx
+++ NEXT/section02/src/pages/index.tsx
@@ -2,10 +2,39 @@ import SearchableLayout from '@/components/searchable-layout';
 import { ReactNode } from 'react';
 import books from '@/mock/books.json';
 import BookItem from '@/components/book-item';
+import { InferGetServerSidePropsType } from 'next';
 
-export default function Home() {
+export const getServerSideProps = () => {
+  const data = 'hello';
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+/*
+getServerSideProps라고 약속된 서버사이드함수
+컴포넌트보다 먼저 실행되어 컴포넌트에 필요한 데이터를 불러오는 함수
+
+export const getServerSideProps = () => {
+return {
+  props: {
+    data,
+  },
+}
+
+무조건 props를 가진 객체를 반환해야 함. 이 형태가 기본
+서버사이드에서만 실행하기 때문에 window.location 등의 프론트 인터렉션이 불가능
+*/
+
+export default function Home({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log(data);
   return (
-    <div className='flex flex-col gap-8 mt-8'>
+    <div className="flex flex-col gap-8 mt-8">
       <section>
         <h2 className="font-bold">New Books</h2>
         {books.map((book) => (
@@ -39,4 +68,10 @@ useEffect(()=>{
 링크 컴퍼넌트의 프리패칭을 해제하려면,
 
 <Link href={'/search'} prefetch={false}>search</Link>
+ */
+
+/* 
+Home 컴포넌트는 서버사이드에서 한 번, js 번들링할 때 한 번 총 두 번 렌더링이 된다
+window 등의 프론트 인터렉션을 그냥 사용할 경우 첫 번째 렌더링에서 오류가 발생한다
+따라서 이런 건 useEffect로 감싸줘야 함
  */
