6b2c3990bc4ac903f93bce91fb5812a70d21a215
Chaewon <cw06155@gmail.com>
2025-02-17 01:04:39 +0900

    app router data fetching

--- NEXT/section03/src/app/(with-searchbar)/page.tsx
+++ NEXT/section03/src/app/(with-searchbar)/page.tsx
@@ -1,20 +1,48 @@
-import books from '@/mock/books.json';
 import BookItem from '@/components/book-item';
+import { BookDataType } from '@/types';
 
-export default function Home() {
+async function AllBooks() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_API_URL}/book`,
+  );
+  if (!response.ok) return <div>오류가 발생했습니다...</div>;
+  const allBooks: BookDataType[] = await response.json();
+
+  return (
+    <div>
+      {allBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
+
+async function RecoBooks() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_API_URL}/book/random`,
+  );
+  if (!response.ok) return <div>오류가 발생했습니다...</div>;
+  const recoBooks: BookDataType[] = await response.json();
+
+  return (
+    <div>
+      {recoBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
+
+export default async function Home() {
   return (
     <div className="flex flex-col gap-8 mt-8">
       <section>
         <h2 className="font-bold">New Books</h2>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <RecoBooks />
       </section>
       <section>
         <h2 className="font-bold">All Books</h2>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <AllBooks />
       </section>
     </div>
   );
--- NEXT/section03/src/app/(with-searchbar)/search/page.tsx
+++ NEXT/section03/src/app/(with-searchbar)/search/page.tsx
@@ -1,14 +1,21 @@
-import books from '@/mock/books.json';
 import BookItem from '@/components/book-item';
+import { BookDataType } from '@/types';
 
 export default async function Search({
   searchParams,
 }: {
-  searchParams: Promise<{ q?: string }>;
+  searchParams: { q?: string };
 }) {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_API_URL}/book/search?q=${searchParams.q}`,
+  );
+
+  if (!response.ok) return <div>오류가 발생했습니다...</div>;
+
+  const searchedData: BookDataType[] = await response.json();
   return (
     <div>
-      {books.map((book) => (
+      {searchedData.map((book) => (
         <BookItem key={book.id} {...book} />
       ))}
     </div>
