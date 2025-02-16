da993b1e7a948e758ff03da6076ad296c2c0e28b
Chaewon <cw06155@gmail.com>
2025-02-17 01:19:47 +0900

    data caching

--- NEXT/section03/src/app/(with-searchbar)/page.tsx
+++ NEXT/section03/src/app/(with-searchbar)/page.tsx
@@ -47,3 +47,14 @@ export default async function Home() {
     </div>
   );
 }
+
+/*
+데이터 캐시(data cache) : 불 필요한 데이터 요청의 수를 줄여서 웹 서비스의 성능을 크게 개선할 수 있음
+const response = await fetch(`~/api`, {cache: options});
+* 오직 fetch 메서드로만 가능. axios 등은 불가능
+
+1. { cache: 'no-store' } : 데이터를 절대 캐싱 x 매번 업데이트 // 기본값
+2. { cache: 'force-cache' } : 요청의 결과를 무조건 캐싱하여 한 번 호출된 이후에는 다시 호출하지 않음 (=SSG)
+3. { next: { revalidate: 10 } } : 일정 주기로 업데이트함 (=ISR)
+4. { next: { tags: ['a'] } } : 트리거가 나타나면 업데이트함 (=ON DEMAND ISR)
+*/
