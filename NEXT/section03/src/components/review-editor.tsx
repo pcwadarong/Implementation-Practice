3d25549135725f6fc4ee72cd816f87f42ff89e52
Chaewon <cw06155@gmail.com>
2025-02-18 00:39:34 +0900

    client component server action

--- NEXT/section03/src/components/review-editor.tsx
+++ NEXT/section03/src/components/review-editor.tsx
@@ -1,9 +1,26 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useActionState } from 'react';
 import { createReviewAction } from '@/actions/create-review-action';
 
 export default function ReviewEditor({ bookId }: { bookId: string }) {
+  // pending 결과, 실행되는 액션, 상태(진행중인지) = useActionState(실행할 것, 초기값)
+  const [state, formAction, isPending] = useActionState(
+    createReviewAction,
+    null,
+  );
+
+  // state는 있지만 status가 false가 될 때마다 (실패할 때마다) alert통해 알림.
+  useEffect(() => {
+    if (state && !state.status) {
+      alert(state.error);
+    }
+  }, [state]);
+
   return (
     <section>
-      <form action={createReviewAction} className="grid grid-cols-6 gap-2">
+      <form action={formAction} className="grid grid-cols-6 gap-2">
         <input name="bookId" value={bookId} hidden readOnly />
         <div className="flex gap-4">
           <label htmlFor="author" className="text-base/10 min-w-fit">
@@ -12,6 +29,7 @@ export default function ReviewEditor({ bookId }: { bookId: string }) {
           <input
             name="author"
             required
+            disabled={isPending}
             placeholder="이름을 입력해주세요."
             className="border p-2 rounded-sm"
           />
@@ -22,14 +40,16 @@ export default function ReviewEditor({ bookId }: { bookId: string }) {
         <textarea
           name="content"
           required
+          disabled={isPending}
           className="border p-2 rounded-sm col-span-6"
           placeholder="주제와 무관한 댓글, 악플 등의 글은 임의로 삭제될 수 있습니다."
         />
         <button
           type="submit"
+          disabled={isPending}
           className="col-end-7 justify-self-end w-fit px-4 py-2 rounded-sm bg-gray-700 text-white"
         >
-          submit
+          {isPending ? '...' : 'submit'}
         </button>
       </form>
     </section>
